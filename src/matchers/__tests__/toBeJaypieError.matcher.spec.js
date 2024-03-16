96b9f14cd6e653dc4a6749fb2c2e542688f75975
Adam Finlayson <amf@adamfinlayson.com>
2024-03-15 23:06:55 -0500

    test: describe tests

--- src/matchers/__tests__/toBeJaypieError.matcher.spec.js
+++ src/matchers/__tests__/toBeJaypieError.matcher.spec.js
@@ -38,25 +38,29 @@ describe("To Be Jaypie Error Matcher", () => {
   it("Is a function", () => {
     expect(toBeJaypieError).toBeFunction();
   });
-  it("Matches instances of Jaypie error objects", () => {
-    const error = new ConfigurationError();
-    const result = toBeJaypieError(error);
-    expect(result.message).toBeFunction();
-    expect(result.message()).toBeString();
-    expect(result.pass).toBeTrue();
+  describe("Error Conditions", () => {
+    it("Rejects instances of plain error objects", () => {
+      const error = new Error(
+        "If this is your first night at Fight Club, you have to fight",
+      );
+      const result = toBeJaypieError(error);
+      expect(result.message).toBeFunction();
+      expect(result.message()).toBeString();
+      expect(result.pass).toBeFalse();
+    });
+    it.todo("Rejects if non-object passed");
+    it.todo("Rejects if no errors array");
+    it.todo("Rejects if errors array is empty");
+    it.todo("Must match the entire json:api error schema");
   });
-  it("Rejects instances of plain error objects", () => {
-    const error = new Error(
-      "If this is your first night at Fight Club, you have to fight",
-    );
-    const result = toBeJaypieError(error);
-    expect(result.message).toBeFunction();
-    expect(result.message()).toBeString();
-    expect(result.pass).toBeFalse();
+  describe("Happy Path", () => {
+    it("Matches instances of Jaypie error objects", () => {
+      const error = new ConfigurationError();
+      const result = toBeJaypieError(error);
+      expect(result.message).toBeFunction();
+      expect(result.message()).toBeString();
+      expect(result.pass).toBeTrue();
+    });
+    it.todo("Matches plain old json errors");
   });
-  it.todo("Matches plain old json errors");
-  it.todo("Rejects if non-object passed");
-  it.todo("Rejects if no errors array");
-  it.todo("Rejects if errors array is empty");
-  it.todo("Must match the entire json:api error schema");
 });

1a6a10ecb3384837331cbb5ebf64854c2612607f
Adam Finlayson <amf@adamfinlayson.com>
2024-03-15 23:29:23 -0500

    feat: export to be jaypie error

--- src/matchers.module.js
+++ src/matchers.module.js
@@ -1,11 +1,14 @@
 import { matchers as jsonSchemaMatchers } from "jest-json-schema";
 
+import toBeJaypieError from "./matchers/toBeJaypieError.matcher.js";
+
 //
 //
 // Export
 //
 
 export default {
+  toBeJaypieError,
   toBeValidSchema: jsonSchemaMatchers.toBeValidSchema,
   toMatchSchema: jsonSchemaMatchers.toMatchSchema,
 };

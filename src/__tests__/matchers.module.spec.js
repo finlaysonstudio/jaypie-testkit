b627772af19d42e740fec2b5940cc4a4f3b74ea2
Adam Finlayson <amf@adamfinlayson.com>
2024-03-15 21:58:41 -0500

    feat: init: matchers module

---
+++ src/__tests__/matchers.module.spec.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line no-unused-vars
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+
+// Subject
+import matchers from "../matchers.module.js";
+
+//
+//
+// Mock constants
+//
+
+//
+//
+// Mock modules
+//
+
+//
+//
+// Mock environment
+//
+
+const DEFAULT_ENV = process.env;
+beforeEach(() => {
+  process.env = { ...process.env };
+});
+afterEach(() => {
+  process.env = DEFAULT_ENV;
+});
+
+//
+//
+// Run tests
+//
+
+describe("Matchers Module", () => {
+  it("Is an object", () => {
+    expect(matchers).toBeObject();
+  });
+});

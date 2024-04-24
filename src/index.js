1c0c18a3a4de851e035ff6f616dba63afae4ebab
Adam Finlayson <amf@adamfinlayson.com>
2024-04-24 10:46:48 -0500

    feat: restore log const

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 //
 //
+// Constants
+//
+
+export const LOG = {
+  LEVEL: {
+    ALL: "all",
+    DEBUG: "debug",
+    ERROR: "error",
+    FATAL: "fatal",
+    INFO: "info",
+    SILENT: "silent",
+    TRACE: "trace",
+    WARN: "warn",
+  },
+};
+
+//
+//
 // Export
 //
 
